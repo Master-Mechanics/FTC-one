41b5e3ec8590fa83203fdbfbeeb5476797fed7a9
1squirtle1 <abhipadala9@gmail.com>
2017-09-17 11:50:49 -0500

    updtaded vuforia, need to test if works

--- ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia.java
+++ ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia.java
@@ -3,7 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
@@ -25,14 +30,25 @@ public class Vuforia extends LinearOpMode
 
     public void runOpMode() throws InterruptedException
     {
+        setupVuforia();
 
-
+        lastKnownLocation = createMatrix(0, 0, 0, 0, 0, 0);
 
         waitForStart();
 
+        visiontargets.activate();
+
         while(opModeIsActive())
         {
+            OpenGLMatrix latestLocation = listener.getUpdatedRobotLocation();
 
+            if(latestLocation != null)
+            {
+                lastKnownLocation = latestLocation;
+            }
+
+            telemetry.addData("Tracking " + target.getName(), listener.isVisible());
+            telemetry.addData("Last Known Location", formatMatrix(lastKnownLocation));
 
             telemetry.update();
             idle();
@@ -44,8 +60,31 @@ public class Vuforia extends LinearOpMode
         parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        vuforiaLocalizer = ClassFactory.createVuforiaLocalizer(parameters);
 
+        visiontargets = vuforiaLocalizer.loadTrackablesFromAsset("FTC_2016-17");
 
+        target = visiontargets.get(0);
+        target.setName("Wheels Targte");
+        target.setLocation(createMatrix(0, 500, 0, 90, 0, 90));
+
+        phoneLocation = createMatrix(0, 225, 0, 90, 0, 0);
+
+        listener = (VuforiaTrackableDefaultListener) target.getListener();
+        listener.setPhoneInformation(phoneLocation, parameters.cameraDirection);
+    }
+
+    public OpenGLMatrix createMatrix(float x, float y, float z, float u, float v, float w)
+    {
+        return OpenGLMatrix.translation(x, y, z).
+                multiplied(Orientation.getRotationMatrix(
+                        AxesReference.EXTRINSIC, AxesOrder.XYX, AngleUnit.DEGREES, u, v, w));
+
+    }
+
+    public String formatMatrix(OpenGLMatrix matrix)
+    {
+        return matrix.formatAsTransform();
     }
 
 
