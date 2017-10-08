ea76c413af7fb187c705d4d94225181b0fb46211
1squirtle1 <abhipadala9@gmail.com>
2017-10-08 10:08:29 -0500

    updtated vuforia, possibly finished

--- ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia.java
+++ ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vuforia.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.vuforia.HINT;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
@@ -27,6 +28,11 @@ public class Vuforia extends LinearOpMode
     OpenGLMatrix phoneLocation;
 
     public static final String VUFORIA_KEY = "AZdKRPL/////AAAAGdWHky5ZjET7mNUJ+qxkIDtvddP9PIXKwsMwBNDXV0SrsOuioPqAv1q7EB2k1QqsJd3eIb8WIXmHt4fxm153DMq6AEpg0L8nuqQuMmzxL9nFfM2ubV6PVSoZcdQqPOwtLpJqk4KUjKc8v0Es0M0aZl7R8a+jyzDiobaKVE9+4EY1dKuRPTB1OYCqCKfn1OkULrWxH6zSzwyf6ztPp6cKFn+oaglcxzX9iOhejsX4CsoAg4X/6HLnWWrux1Z4hfVP2EKanjrXqeSm5iursV0Lu+HRKMvo9VbIzGnit7JDusQsI7JVEpVsRnR39GaTJT1W1jijOVaw7VVkCt/7D9U+z5JcjgzOoug+Mye9H6n020MW";
+
+    private float robotX = 0;
+    private float robotY = 0;
+    private float robotAngle = 0;
+
 
     public void runOpMode() throws InterruptedException
     {
@@ -60,9 +66,11 @@ public class Vuforia extends LinearOpMode
         parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parameters.useExtendedTracking = false;
         vuforiaLocalizer = ClassFactory.createVuforiaLocalizer(parameters);
 
         visiontargets = vuforiaLocalizer.loadTrackablesFromAsset("FTC_2016-17");
+        com.vuforia.Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
 
         target = visiontargets.get(0);
         target.setName("Wheels Targte");
