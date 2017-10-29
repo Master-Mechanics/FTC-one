293dd83ee5654341abf6936f67fc3c4cc8c5befd
prydt <prydt@waifu.club>
2017-10-29 12:43:41 -0500

    made right joystick main controller

--- ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpWithArm.java
+++ ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpWithArm.java
@@ -1,33 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
-import com.sun.tools.javac.util.ForwardingDiagnosticFormatter;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+@TeleOp(name="TeleOp w/ Arm")
+public class TeleOpWithArm extends OpMode{
 
-@TeleOp(name="TeleOp")
-public class TeleOpDefault extends OpMode{
-
-    DcMotor ld = null, rd = null;
+    DcMotor ld = null, rd = null, arm = null;
 
     public void init() {
         ld = hardwareMap.get(DcMotor.class, "left_drive");
         rd = hardwareMap.get(DcMotor.class, "right_drive");
+        arm = hardwareMap.get(DcMotor.class, "front_arm");
 
         ld.setDirection(DcMotorSimple.Direction.REVERSE);
         rd.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     public void loop() {
-        double drive = -gamepad1.right_stick_y, turn = gamepad1.right_stick_x;
+        double  drive = -gamepad1.right_stick_y, turn = gamepad1.right_stick_x;
+
 
         ld.setPower(Range.clip(drive + turn, -1d, 1d));
         rd.setPower(Range.clip(drive - turn, -1d, 1d));
+
+        arm.setPower(gamepad1.left_stick_y * .2);
     }
 }
