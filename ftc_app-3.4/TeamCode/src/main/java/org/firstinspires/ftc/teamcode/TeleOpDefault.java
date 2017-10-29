e084ba7a3cece0fb8977cd06e04e0a5c847aa91d
prydt <prydt@waifu.club>
2017-10-29 10:25:23 -0500

    added basic single joystick controls

--- ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDefault.java
+++ ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDefault.java
@@ -1,95 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
-/**
- * Created by Arindum on 10/22/2017.
- */
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
+import com.sun.tools.javac.util.ForwardingDiagnosticFormatter;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 
 @TeleOp(name="TeleOp")
 public class TeleOpDefault extends OpMode{
 
-
-    org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot robot = new HardwarePushbot();
-    DcMotor ld, rd;
-
-    enum Mode {
-        tank,
-        singleJoystick
-    }
-
-    Mode mainMode = Mode.singleJoystick;
+    DcMotor ld = null, rd = null;
 
     public void init() {
-        robot.init(hardwareMap);
         ld = hardwareMap.get(DcMotor.class, "left_drive");
         rd = hardwareMap.get(DcMotor.class, "right_drive");
+
+        ld.setDirection(DcMotorSimple.Direction.REVERSE);
+        rd.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     public void loop() {
-        double  left_y  = gamepad1.left_stick_y,
-                left_x  = gamepad1.left_stick_x,
-                right_y = gamepad1.left_stick_y,
-                right_x = gamepad1.right_stick_x,
-                left = 0d,
-                right = 0d;
+        double drive = -gamepad1.left_stick_y, turn = gamepad1.left_stick_x;
 
-        switch(mainMode)
-        {
-            case tank:
-                if(left_y > 0 && right_y > 0)
-                {
-                    left    = (left_y + right_y) / 2;
-                    right   = (left_y + right_y) / 2;
-                }
-                else if(left_y < 0 && right_y < 0)
-                {
-                    left    = (left_y + right_y) / 2;
-                    right   = (left_y + right_y) / 2;
-                }
-                else if(left_y < 0 && right_y > 0)
-                {
-                    double avg = (Math.abs(left_y) + Math.abs(right_y)) / 2;
-                    left = avg;
-                    right = -1 * avg;
-                }
-                else if(left_y > 0 && right_y < 0)
-                {
-                    double avg = (Math.abs(left_y) + Math.abs(right_y)) / 2;
-                    left = -1 * avg;
-                    right = avg;
-                }
-                break;
-            case singleJoystick:
-                if((left_y > 0 || left_y < 0) && left_x == 0)
-                {
-                    left    = left_y;
-                    right   = left_y;
-                }
-                else
-                {
-                    if(left_x > 0)
-                    {
-                        left    = left_y;
-                        right   = -1 * left_y;
-                    }
-                    else if(left_x < 0)
-                    {
-                        left    = -1 * left_y;
-                        right   = left_y;
-                    }
-                }
-                break;
-        }
-
-        ld.setPower(left);
-        rd.setPower(right);
+        ld.setPower(Range.clip(drive + turn, -1d, 1d));
+        rd.setPower(Range.clip(drive - turn, -1d, 1d));
     }
 }
